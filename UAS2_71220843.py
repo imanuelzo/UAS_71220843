e12d3d9fc8b7a0692681fbd651fed3ffbeeab140
imanuelzo <117885348+imanuelzo@users.noreply.github.com>
2023-01-03 12:25:17 +0700

    Add files via upload

---
+++ UAS2_71220843.py
@@ -0,0 +1,17 @@
+import turtle
+p=turtle
+p.bgcolor("pink")
+p.pensize(3)
+
+p.fillcolor("yellow")
+p.begin_fill()
+for p in range (6):
+    p.left(90)
+p.forward(180)
+p.right(90)
+p.forward(30)
+p.right(45)
+p.forward(180)
+p.right(90)
+p.forwae
+turtle.done()
